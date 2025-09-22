a16b0bc3eb3a8cf900cd30b65de543ad2a04f23b
jagunlee <jagunlee@postech.ac.kr>
2025-09-22 21:45:19 +0900

    simple strategy

--- data.py
+++ data.py
@@ -22,7 +22,13 @@ class Diag:
 class Triangulation:
     def __init__(self, pts, T):
         self.pts = pts
+        self.edges: dict = dict()
         self.make_triangulation(T)
+
+    # def __init__(self, pts, edges:dict):
+    #     self.pts = pts
+    #     self.edges = edges
+
         
     def make_triangulation(self, T):
         edges = dict()
@@ -75,15 +81,27 @@ class Triangulation:
 
         
     def intersect(self, d11, d12, d21, d22):
+        def _orient(a, b, c) -> float:
+            return (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x)
         # (d11,d12)와 (d21,d22)가 intersect하는지 확인
+        EPS = 1e-9
         p1 = self.pts[d11]
         p2 = self.pts[d12]
         p3 = self.pts[d21]
         p4 = self.pts[d22]
-        x1 = ((p2.y-p1.y)*(p4.x-p3.x)*p1.x-(p2.x-p1.x)*(p4.x-p3.x)*p1.y-(p4.y-p3.y)*(p2.x-p1.x)*p3.x+(p4.x-p3.x)*(p2.x-p1.x)*p3.y)/((p2.y-p1.y)*(p4.x-p3.x)-(p4.y-p3.y)*(p2.x-p1.x))
-        if p1.x<x1 and x1<p2.x:
+        o1 = _orient(p1, p2, p3)
+        o2 = _orient(p1, p2, p4)
+        o3 = _orient(p3, p4, p1)
+        o4 = _orient(p3, p4, p2)
+
+        # 일반적인 교차 (서로 다른 편에 위치)
+        if (o1 * o2 < -EPS) and (o3 * o4 < -EPS):
             return True
         return False
+            # x1 = ((p2.y-p1.y)*(p4.x-p3.x)*p1.x-(p2.x-p1.x)*(p4.x-p3.x)*p1.y-(p4.y-p3.y)*(p2.x-p1.x)*p3.x+(p4.x-p3.x)*(p2.x-p1.x)*p3.y)/((p2.y-p1.y)*(p4.x-p3.x)-(p4.y-p3.y)*(p2.x-p1.x))
+        # if p1.x<x1 and x1<p2.x:
+        #     return True
+        # return False
     
     def is_convex_quad(self, i, j):
         # (i,j) edge가 실제로 존재하고 flip 가능한지 확인
@@ -97,6 +115,37 @@ class Triangulation:
             
         except:
             return False
+        
+    def maximal_disjoint_convex_quad(self, E:list[(int, int)], prev_use=[]):
+        res = []
+        second_best = None
+        used = [False]*len(self.pts)
+        for i, e in enumerate(E):
+            if self.is_convex_quad(e[0], e[1]):
+                if e not in prev_use:
+                    res.append(e)
+                    used[e[0]] = True
+                    used[e[1]] = True
+                    used[self.edges[e].nei_pts[0]] = True
+                    used[self.edges[e].nei_pts[1]] = True
+                    break
+                else:
+                    second_best = e
+        if not res:
+            res.append(e)
+            used[e[0]] = True
+            used[e[1]] = True
+            used[self.edges[e].nei_pts[0]] = True
+            used[self.edges[e].nei_pts[1]] = True
+        for j in range(len(E)):
+            if self.is_convex_quad(e[0], e[1]):
+                check_ind = [E[j][0],E[j][1],self.edges[E[j]].nei_pts[0],self.edges[E[j]].nei_pts[1]]
+                check_use = [used[k] for k in check_ind]
+                if True in check_use:
+                    continue
+                for ind in check_ind: used[ind] = True
+                res.append(E[j])
+        return res
         
     def flip(self, i, j):
         # (i,j) edge가 flip 가능하면 flip하고 새로 생긴 edge return, 아니면 (-1,-1) return
@@ -129,16 +178,20 @@ class Triangulation:
             self.edges[d2, d1] = Diag(d2, d1)
             self.edges[d2, d1].nei_pts = [i,j]
         del self.edges[(min(i,j),max(i,j))]
-        return d1, d2
+        return min(d1, d2),max(d1, d2)
         
     def find_difference(self, T):
         e1 = set(self.edges.keys())
         e2 = set(T.edges.keys())
+        return (list(e1-e2), list(e2-e1))
         
     def check_1pfd(self, T):
+        E1, E2 = self.find_difference(T)
+        E1_in = self.maximal_disjoint_convex_quad(E1)
+        if len(E1_in)==len(E1): 
+            return True
+        return False
         # T와 1 parallel flip distance인지 확인
-        pass
-        
         
     
 class Data:
@@ -169,14 +222,23 @@ class Data:
             print(f"num of triangulations: {len(self.triangulations)}")
         for i in range(len(self.triangulations)):
             self.DrawTriangulation(self.triangulations[i], name = f"{i}")
+        T:Triangulation = self.triangulations[0]
+        step = 0
+        res_e_list = []
         while True:
-            i,j = random.randint(0,len(self.pts)-1), random.randint(0,len(self.pts)-1)
-            if i==j:
-                continue
-            ans = self.triangulations[0].flip(i,j)
-            if ans[0]>=0:
+            step+=1
+            E1, E2 = T.find_difference(self.triangulations[1])
+            if not E1:
                 break
-        self.DrawTriangulation(self.triangulations[0], colored_edges=[(min(ans), max(ans))],name=f"{i} {j}")
+            e_list = T.maximal_disjoint_convex_quad(E1, res_e_list)
+            if not e_list:
+                pdb.set_trace()
+            res_e_list = []
+            for e in e_list:
+                res_e_list.append(T.flip(e[0],e[1]))
+            self.DrawTriangulation(T, colored_edges=res_e_list,name=f"step {step}")
+            self.DrawTriangulation(T, colored_edges=res_e_list,name=f"check")
+            # pdb.set_trace()
         # for e in self.triangulations[0].edges.values():
         #     print(f"edge {e.p1} {e.p2}: {e.nei_pts[0]}, {e.nei_pts[1]}")
         
