63e8330bc0330a28812b5def6d46b0e514bfaa29
jagunlee <jagunlee@postech.ac.kr>
2025-10-12 15:02:48 +0900

    Add writefile

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import os
 from data import *
 import time
 
@@ -9,9 +10,16 @@ if __name__=="__main__":
     else:
         # inp = "data/examples/example_ps_20_nt2_pfd5_random.json"
         inp = "data/examples/example_ps_200_nt16_pfd40_random.json"
-    start = time.time()
-    dt = Data(inp)
-    dt.find_center()
-    end = time.time()
-    print(f"total time: {end-start}s")
-    dt.random_move()
+    if "json" in inp:
+        start = time.time()
+        dt = Data(inp)
+        dt.find_center()
+        end = time.time()
+        print(f"total time: {end-start}s")
+        dt.random_move()
+    else:
+        json_list = os.listdir(inp)
+        for inp1 in json_list:
+            dt = Data(inp1)
+            dt.find_center()
+            dt.WriteData()
