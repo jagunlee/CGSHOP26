78394fcf1aa426882a81282ca0aab3360fe58da1
hwikim <51110494+hwikim@users.noreply.github.com>
2025-11-23 13:51:13 +0900

    Make coreset on Points

---
+++ build/src/cgshop2026_pyutils/geometry/CMakeFiles/_bindings.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/ALGO_SERVER/Desktop/PFD/CGSHOP26/pyutils26/src/cgshop2026_pyutils/geometry/_bindings.cpp" "src/cgshop2026_pyutils/geometry/CMakeFiles/_bindings.dir/_bindings.cpp.obj" "gcc" "src/cgshop2026_pyutils/geometry/CMakeFiles/_bindings.dir/_bindings.cpp.obj.d"
+  "C:/Users/ALGO_SERVER/Desktop/PFD/CGSHOP26/pyutils26/src/cgshop2026_pyutils/geometry/cgal_utils.cpp" "src/cgshop2026_pyutils/geometry/CMakeFiles/_bindings.dir/cgal_utils.cpp.obj" "gcc" "src/cgshop2026_pyutils/geometry/CMakeFiles/_bindings.dir/cgal_utils.cpp.obj.d"
+  "C:/Users/ALGO_SERVER/Desktop/PFD/CGSHOP26/pyutils26/src/cgshop2026_pyutils/geometry/geometry_operations.cpp" "src/cgshop2026_pyutils/geometry/CMakeFiles/_bindings.dir/geometry_operations.cpp.obj" "gcc" "src/cgshop2026_pyutils/geometry/CMakeFiles/_bindings.dir/geometry_operations.cpp.obj.d"
+  "C:/Users/ALGO_SERVER/Desktop/PFD/CGSHOP26/pyutils26/src/cgshop2026_pyutils/geometry/triangulation_validation.cpp" "src/cgshop2026_pyutils/geometry/CMakeFiles/_bindings.dir/triangulation_validation.cpp.obj" "gcc" "src/cgshop2026_pyutils/geometry/CMakeFiles/_bindings.dir/triangulation_validation.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
