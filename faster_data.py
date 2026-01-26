b4b6835c6de4a4a21b184a9127252a6c9afe34a2
sloth <taehoon@sookmyung.ac.kr>
2026-01-27 00:10:42 +0900

    fixed

--- faster_data.py
+++ faster_data.py
@@ -730,7 +730,7 @@ class FastData:
                     escore=0
                     for j in range(num):
                         if i==j: continue
-                        score, _ = self.flip_score(tri, mtriangulations[j], e, 1)
+                        score, _ = self.flip_score(tri, mtriangulations[j], e, 1, 1)
                         escore +=score
                     if escore >0:
                         ncand.append((e, escore))
@@ -766,6 +766,106 @@ class FastData:
             if i< num-1:
                 assert(mtriangulations[i].edges == mtriangulations[i+1].edges)
             self.pFlips.append(pfps[i])
+        return mtriangulations[0]
+
+    def findCenterGlobal2(self):
+        # if self.log:
+        #     start = time.time()
+        mtriangulations = [copy.deepcopy(t) for t in self.triangulations]
+        num = len(mtriangulations)
+        pfps =[[] for _ in range(num)]
+        scores = [dict() for _ in range(num)]
+        tl = 0
+        for i in range(num):
+            tri = mtriangulations[i]
+            edges = list(tri.edges)
+            for e in edges:
+                if not self.flippable(tri, e): continue
+                escore = 0
+                for j in range(num):
+                    if i==j: continue
+                    score, _ = self.flip_score(tri, mtriangulations[j], e, 1, 1)
+                    escore += score
+                scores[i][e] = escore
+            # if self.log:
+            #     print(i, "/", num)
+            #     end = time.time()
+            #     print('time:', f"{end - start:.5f} sec")
+        while True:
+            mscore = 0
+            for i in range(num):
+                tri = mtriangulations[i]
+                ncand = []
+                nflips = []
+                nscore = 0
+                for e in scores[i]:
+                    if scores[i][e] > 0:
+                        ncand.append((e, scores[i][e]))
+                ncand.sort(key=lambda x:x[1], reverse=True)
+                marked = set()
+                for (p1, p2), score in ncand:
+                    t1 = tri.find_face(p1, p2)
+                    t2 = tri.find_face(p2, p1)
+                    if t1 in marked or t2 in marked:
+                        continue
+                    nflips.append((p1, p2))
+                    marked.add(t1)
+                    marked.add(t2)
+                    nscore += score    
+                if nscore > mscore:
+                    mscore = nscore
+                    mi = i
+                    mflips = nflips
+            if mscore == 0:
+                break
+            tl+=1
+            # if self.log:
+            #     print(tl)
+            #     print(mscore)
+            #     end = time.time()
+            #     print('time:', f"{end - start:.5f} sec")
+            pfps[mi].append(mflips)
+            for i in range(num):
+                if i == mi:
+                    continue
+                tri = mtriangulations[i]
+                edges = list(tri.edges)
+                for e in edges:
+                    if not self.flippable(tri, e): continue
+                    score, _ = self.flip_score(tri, mtriangulations[mi], e, 1, 1)
+                    scores[i][e] -= score
+            for e in mflips:
+                mtriangulations[mi].flip(*e)
+            for i in range(num):
+                if i == mi:
+                    scores[i].clear()
+                    tri = mtriangulations[i]
+                    edges = list(tri.edges)
+                    for e in edges:
+                        if not self.flippable(tri, e): continue
+                        escore = 0
+                        for j in range(num):
+                            if i==j: continue
+                            score, _ = self.flip_score(tri, mtriangulations[j], e, 1, 1)
+                            escore += score
+                        scores[i][e] = escore
+                else:
+                    tri = mtriangulations[i]
+                    edges = list(tri.edges)
+                    for e in edges:
+                        if not self.flippable(tri, e): continue
+                        score, _ = self.flip_score(tri, mtriangulations[mi], e, 1, 1)
+                        scores[i][e] += score
+        self.pFlips = []
+        for i in range(num):
+            if i < num-1:
+                assert(mtriangulations[i].edges == mtriangulations[i+1].edges)
+            self.pFlips.append(pfps[i])
+        
+        # if self.log: print("total length:",tl)
+        while len(mtriangulations) > 1:
+            tri = mtriangulations.pop()
+            del tri
         return mtriangulations[0]
 
     def flip_rev(self, tri, e):
@@ -808,7 +908,7 @@ class FastData:
                 newD.triangulations.append(newT)
             start=time.time()
             # print("findCenterGlobal() takes ... ", end=' ', flush=True)
-            self.center = newD.findCenterGlobal()
+            self.center = newD.findCenterGlobal2()
             # print(f"{time.time()-start:.2f}s")
             for i in range(self.num_tris):
                 self.pFlips[i] = self.pFlips[i][:-revnum[i]] + newD.pFlips[i]
